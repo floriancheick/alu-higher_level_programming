df1487c394efdccbd4cbc1050e8d7e6910657782
root <floriancheick>
2022-07-08 11:14:33 -0700

    100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-from sys import argv
 if __name__ == "__main__":
-    if len(argv) != 4:
-        print("Usage:", argv[0], "<a> <operator> <b>")
-        exit(1)
-        opr = argv[2]
-    opt = {"+": add, "-": sub, "*": mul, "/": div}
-    if opr not in opt:
+    import calculator_1 as extra
+    import sys
+argNum = len(sys.argv)  # gets number of arguments
+la = sys.argv  # stores arguments return by sys module
+if argNum != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
+else:
+    a = int(la[1])
+    b = int(la[3])
+    if la[2] == "-":
+        print("{:d} - {:d} = {:d}".format(a, b, extra.sub(a, b)))
+    elif la[2] == "*":
+        print("{:d} * {:d} = {:d}".format(a, b, extra.mul(a, b)))
+    elif la[2] == "/":
+        print("{:d} / {:d} = {:d}".format(a, b, extra.div(a, b)))
+    elif la[2] == "+":
+        print("{:d} + {:d} = {:d}".format(a, b, extra.add(a, b)))
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-        a = int(argv[1])
-    b = int(argv[3])
-    print("{:d} {:s} {:d} = {:d}".format(a, opr, b, opt[opr](a, b)))
