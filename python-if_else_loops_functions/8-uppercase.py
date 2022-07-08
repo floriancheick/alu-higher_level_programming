d49dd2a03d556c776df46b82dab93a461ffda8f4
root <floriancheick>
2022-07-08 00:29:55 -0700

    8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-value = -32
-def upppercase(str):
-    i =0
-    while i < len(str):
-        if islower(str[i]):
-            lower = ord(str[1]) + value
-            new = chr(lower)
-        else:
-            new = str[1]
-            print("{}".format(new),  end= '')
-            i = i + 1
-            print("")
-            def islower(str):
-                value1 = ord(str)
-                if value1 >= 97 and value1 <= 122:
-                    return True
-                else:
-                    return False
+def uppercase(str)
+uppercase = ''
+for i in str:
+    if (i >= 'a' and i <= 'z'):
+        uppercase = uppercase + chr((ord(i) -32))
+    else:
+        uppercase = uppercase + 1
+        print("{}".format(uppercase))
