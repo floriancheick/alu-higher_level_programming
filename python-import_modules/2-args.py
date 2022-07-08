e769aeaba6cdb6e152aa9735a6f10eaa47030942
root <floriancheick>
2022-07-08 10:58:05 -0700

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    entry = argv[1:]
-    size = len(entry)
-    print("{:d} {:s}{:s}".format(size, "argumets" if size !=1 else "arguments", "." if size == 0 else ":"))
-    for count, arg in enumerate(entry, 1):
-        print("{:d}: {:s}".format(count, arg))
+import sys
+def main():
+    args = sys.argv[1:]
+    args_len = len(args)
+    if args_len == 0:
+        print("0 arguments.")
+    elif args_len == 1:
+        print("1 argument:\n1:{}".format(args[0]))
+    else:
+        print("{:d} arguments:".format(args_len))
+        for i in range(args_len):
+            print("{:d}: {}".format(i + 1, ags[i]))
+            if __name__ == "__main__": main()
