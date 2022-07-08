454e7c6522d6b66daa8a007ccf93ed110554c666
root <floriancheick>
2022-07-08 11:10:33 -0700

    100-my_calculator.py

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage:", argv[0], "<a> <operator> <b>")
+        exit(1)
+        opr = argv[2]
+    opt = {"+": add, "-": sub, "*": mul, "/": div}
+    if opr not in opt:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+        a = int(argv[1])
+    b = int(argv[3])
+    print("{:d} {:s} {:d} = {:d}".format(a, opr, b, opt[opr](a, b)))
