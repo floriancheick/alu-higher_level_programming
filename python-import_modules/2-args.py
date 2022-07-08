db1548c96e23c8df93af105ef229d02a4e0be1d8
root <floriancheick>
2022-07-08 10:13:13 -0700

    2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    value = sys.argv
+    number = len(value)
+    i = 1
+    if number == 1:
+        print("0 arguments.")
+    elif number == 2:
+        print("1 arguments:")
+        print("1: {}".format(value[1]))
+    elif number > 2:
+        print("{:d} arguments:".format(number -1))
+        while i < number:
+            print("{:d}: {}".format((i), values[i]))
+            i = i + 1
