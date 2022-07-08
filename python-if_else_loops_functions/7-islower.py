7aa9c75198a6af83a8e2f884728829319987ebf8
root <floriancheick>
2022-07-07 23:55:33 -0700

    7-islower.py

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    value = ord(c)
-    if value >= 97 and value <= 122:
-        return True
-    elif value >=48 and value <= 57:
-        return False
-    elif value >=65 and value <= 90:
-        return False
+    return (ord('a') <= ord(c) <= ord('z'))
