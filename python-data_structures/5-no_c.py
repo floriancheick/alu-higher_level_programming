aa6864cc28afb62c0aa0511d0652a42476b24713
root <floriancheick>
2022-07-14 13:43:11 -0700

    5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_str = ''
-    for i in range (len(my_string)):
-        if my_string[i] == 'C' or my_string[i] == 'c' :
-            continue
-        my_str = my_str + my_string[i]
-        my_string = my_str
-        return my_string
+    return (my_string.translate({ord('C'): None,  ord('c'): None}))
