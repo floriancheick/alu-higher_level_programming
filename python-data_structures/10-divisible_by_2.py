7358b709f6566904ce43919e36711287bbb1db31
root <floriancheick>
2022-07-15 03:23:12 -0700

    10-divisible_by_2.py

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-        if my_list:
-                list_one = []
+        list_one = []
+        if list_one:
                 for i in my_list:
-                        if i % 2 == 0:
-                                list_one.append(True)
-                        else:
-                                list_one.append(False)
-                                return list_one
+                        list_one.append(False if i % 2 else True)
+                        return list_one
