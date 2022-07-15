ce6ac4a59eb67f52386b1713bb6c72cb4e365e0c
root <floriancheick>
2022-07-15 07:22:29 -0700

    12-roman_to_init.py

--- python-more_data_structures/12-roman_to_init.py
+++ python-more_data_structures/12-roman_to_init.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (type(roman_string) is not str) or roman_string is None:
+    if not isinstance(roman_string, str):
         return 0
-    if type(roman_string) is str:
-        nar = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        add = 0
-        for x, y in zip(roman_string, roman_string[1:]):
-            if x not in nar.keys():
-                return 0
-            elif nar[x] >= nar[y]:
-                add += nar[x]
-            else:
-                add -= nar[x]
-        add += nar[roman_string[-1]]
-        return add
+    my_list = 0
+    number = 0
+    roman_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for r in reversed(roman_string):
+        number = digits[r]
+        my_list += number if my_list < number * 5 else -number
+    return my_list
