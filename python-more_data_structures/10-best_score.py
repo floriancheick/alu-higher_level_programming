2c79c8e63b8a233d7ddec6d47100075e56848ba6
root <floriancheick>
2022-07-15 07:38:56 -0700

    10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is not None:
         for key, val in a_dictionary.items():
-            if val == ax(a_dictionary.values()):
+            if val == max(a_dictionary.values()):
                 return key
-            else:
-                return None
+    else:
+        return None
