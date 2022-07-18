b587557e0da01b32cebe8496e916aa6976a398f7
root <floriancheick>
2022-07-18 11:29:32 -0700

    2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,9 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     idex = 0
     for i in range(x):
         try:
-            print("{:d}".foramt(my_list[i], end='')
-                  idex += 1
-                  except(TypeError, valueError):
-                  continue
-                  print()
-                  return idex
+            print("{:d}".format(my_list[i]), end='')
+            idex += 1
+        except(TypeError, ValueError):
+            continue
+    print()
+    return idex
