7fac4e6479a3ab3d4e4eebaef13320c5ddc277ce
root <floriancheick>
2022-07-21 16:31:05 -0700

    6-square.py

---
+++ python-classes/6-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+class Squaere is define in this modulr
+"""
+
+
+class Square:
+    """
+    Args: size(int) 
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) != tuple or len(value) != 2 or \
+           type(value[0]) != int or type(value[1]) != int or \
+           value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return (self.__size)**2
+
+    def my_print(self):
+        if self.__size != 0:
+            for j in range(self.__position[1]):
+                print()
+            for row in range(self.__size):
+                for i in range(self.__position[0]):
+                    print(" ", end="")
+                for column in range(self.__size):
+                    print("#", end="")
+                print()
+        else:
+            print()
