2771f57fc96a212d99f677fb40c7f8d1d4dff7de
root <floriancheick>
2022-07-21 14:51:58 -0700

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+1-square.py module define class square 
+"""
+class Square:
+    """
+class create a private size instance
+"""
+    def __init__(self, size):
+        self.Square__size = size
