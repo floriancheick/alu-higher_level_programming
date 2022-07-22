dc9fac9cc878d8faa551d8ee6c6af1ae3ef17776
root <floriancheick>
2022-07-22 01:09:24 -0700

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,11 +3,11 @@
 This Module defines a class Square with private instance attribute
 """
 
+
 class Square:
     """
     Args:size -class Square definition
     """
-    
     def __init__(self, size):
         """
         Attributes:size
