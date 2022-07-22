56c35c0042fbe95364f395e4f5d234341e1cb11f
root <floriancheick>
2022-07-22 01:27:51 -0700

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-'''
-Class Square is define in thids module
-'''
-class Square:
-    '''
-class assigns the size of square
-'''
-    def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size  < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+"""
+This module define a class Square 
+"""
+class Square
+"""
+Args:size(int) 
+"""
+def __init__(self, size=0):
+    """
+Attributes:__size(int)
+"""
+    if type(size) is not int:
+        raise  TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    else:
+        self.__size = size
