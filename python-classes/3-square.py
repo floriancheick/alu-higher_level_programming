650ef8a19d7a9d163ef4e0a5b8eb0cfc2958b766
root <floriancheick>
2022-07-22 04:25:08 -0700

    3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""
-This module define class square
-"""
+"""This module define class Square."""
 class Square:
-    """
-class Square
-"""
+    """Present a square."""
     def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
+        """New square.
+Args:
+size (int):size of new square.
+"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an  integer")
         elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-            def area(self):
-                return (self.__size) ** 2
+            raise ValueError("size must be >=0")
+        self.__size = size
+        def area(self):
+            """Return the current square area."""
+            return (self.__size * self.__size)
