ef3915374d5c8e38649f2d76e3181fd8f76e78c8
root <floriancheick>
2022-07-22 06:48:24 -0700

    5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
-"""
-This module define class square
-"""
+"""This module define class square."""
 class Square:
-    """
-Args:size(int) -class Square definition
-"""
+    """Present a square."""
     def __init__(self, size=0):
+        """new square
+Args: size (int): size of new square.
+"""
         self.size = size
         @property
         def size(self):
-            return self.__size
+            """set current size of the square."""
+            return (self.__size)
         @size.setter
         def size(self, value):
-            if type (value) is not init:
+            if not isinstance(value, int):
                 raise TypeError("size must be an integer")
-            elif value < 0:
+            elif value <0:
                 raise ValueError("size must be >= 0")
-            else:
-                self.__size = value
-                def area(self):
-                    retun(self.size)**2
-                    def my_print(self):
-                        if self.__size !=0:
-                            for row in range(self.__size):
-                                print("#", end="")
-                                print()
-                            else:
-                                print()
+            self.__size = value
+            def area(self):
+                """Return current square area."""
+                return(self.__size * self.__size)
