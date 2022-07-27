62d7960dfe21fa92d4a167908f05972575198548
root <floriancheick>
2022-07-27 07:35:36 -0700

    100-magic_string.py

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string_c = getattr(magic_string, 'n', 0) + 1
-    return ("Holberton, " * (magic_string_c - 1) + "Holberton")
+def magic_string(stg=[]):
+    stg += ["Holberton"]
+    return ", ".join(stg)
