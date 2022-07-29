f85bdf550ffd42fae46c1f8188dae3509a281180
root <floriancheick>
2022-07-29 08:25:29 -0700

    9-rectangle.py

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -8,15 +8,11 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """Initialize a new Rectangle"""
-        super().integer_validator("width", width)
         self.__width = width
-        super().integer_validator("height", height)
         self.__height = height
-
+        BaseGeometry.integer_validator(self, "width", self.__width)
+        BaseGeometry.integer_validator(self, "height", self.__height)
         def area(self):
-            """Return the rectangle area."""
-            return (self.__width * self.__height)
-
+            return self.__width * self.__height
         def __str__(self):
-            """Return  the print() and str()"""
-            return ("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
+            return ("[Rectangle] " + str(self.__width) + "/" + str(self.__height))
