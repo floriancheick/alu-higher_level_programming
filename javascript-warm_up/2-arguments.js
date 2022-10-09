86881538446c0921b0c19943f89a351d525d54bd
root <f.nganji@alustudent.com>
2022-10-09 13:34:24 -0700

    2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
-if (process.argv.length <= 2) {
-    console.log('No argument');
-} else if (process.argv.length == 3) {
-    console.log('Argument found');
+const args = process.argv;
+if (args.length === 3) {
+  console.log('Argument found');
+} else if (args.length > 3) {
+  console.log('Arguments found');
 } else {
-    console.log('Argument found');
+  console.log('No argument');
 }
